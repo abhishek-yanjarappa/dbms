e45c444151db828185039714f204f154e5db3fa3
abhishek-yanjarappa <75441358+abhishek-yanjarappa@users.noreply.github.com>
2023-01-02 21:01:27 +0530

    initial commit

---
+++ site/app/head.tsx
@@ -0,0 +1,9 @@
+export default function Head() {
+  return (
+    <>
+      <title>DBMS Project</title>
+      <meta content="width=device-width, initial-scale=1" name="viewport" />
+      <meta name="description" content="Customer relationship management." />
+    </>
+  );
+}
---
+++ site/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main>
+      <h1>namaste</h1>
+    </main>
+  );
+}
