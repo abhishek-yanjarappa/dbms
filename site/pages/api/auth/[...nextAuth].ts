3d74c05c945cd209ecd800b3d6c4537b2a4ce54d
abhishek-yanjarappa <75441358+abhishek-yanjarappa@users.noreply.github.com>
2023-01-13 13:25:46 +0530

    one sec

--- site/pages/api/auth/[...nextAuth].ts
+++ site/pages/api/auth/[...nextAuth].ts
@@ -1,8 +1,10 @@
 import NextAuth from "next-auth";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
 
 export const authOptions = {
   // Configure one or more authentication providers
   site: process.env.NEXTAUTH_URL,
+  adapter: PrismaAdapter(prisma),
   providers: [],
   callbacks: {
     async session({ session, token }: any) {
